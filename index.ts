bbf92e43c62b24f82ba03885894be9199b6433ba
Kyrylo Biloborodko <kyrylo_biloborodko@epuspriw000d.lan>
2023-05-27 19:04:51 -0400

    Set up automatic s3 bucket creation

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import {FmBucket} from "./resources/bucket";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
+new FmBucket({
+    Name: "example",
+    Product: "devops-course"
+});
